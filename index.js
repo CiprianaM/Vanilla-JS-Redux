92d0e4318e97ffd957bcc151588f0a5b3d9976dd
CiprianaM <cipriana2001@gmail.com>
2020-04-22 17:21:06 +0200

    import action creators

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const store = require('./redux');
-console.log(store)
+const {changeCount, double, halve} = require ('./redux/count')
+const {addFavoriteThing, removeFavoriteThing} = require ('./redux/favoriteThings');
+const {updateYoutubeVideoTitle, upvoteYoutubeVideo, downvoteYoutubeVideo} = require ('./redux/youtubeVideo');
+console.log(changeCount)
+
+store.dispatch({type: 'CHANGE_COUNT', payload: 22})
+store.dispatch(changeCount(-53))
+store.dispatch(addFavoriteThing('puppers'))
+store.dispatch(addFavoriteThing('kittens'))
+store.dispatch(addFavoriteThing('more puppers'))
+store.dispatch(addFavoriteThing('snackies'))
+store.dispatch(removeFavoriteThing('kittens'))
+store.dispatch(double())
+store.dispatch(downvoteYoutubeVideo())
+store.dispatch(downvoteYoutubeVideo())
+store.dispatch(upvoteYoutubeVideo())
+store.dispatch(updateYoutubeVideoTitle('Its a wonderful life'))
+store.dispatch(halve())
