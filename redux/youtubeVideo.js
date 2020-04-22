f71590bcac1eafa0bfa71876abec8ac22c4c3a58
CiprianaM <cipriana2001@gmail.com>
2020-04-22 16:57:41 +0200

    create youtubeVideo reducer

---
+++ redux/youtubeVideo.js
@@ -0,0 +1,52 @@
+
+export function upvoteYoutubeVideo () {
+  return {
+    type: 'UPVOTE_YOUTUBE_VIDEO',
+  }
+}
+
+export function downvoteYoutubeVideo () {
+  return {
+    type: 'DOWNVOTE_YOUTUBE_VIDEO',
+  }
+}
+
+const initialState = {
+  title: '',
+  viewCount: 0,
+  votes: {
+    up: 0,
+    down: 0
+  }
+}
+
+export default function youtubeVideoReducer (youtubeVideo = initialState, action) {
+  switch (action.type) {
+    case 'UPDATE_YOUTUBE_VIDEO_TITLE':
+      return {
+        ...youtubeVideo,
+        title: action.payload
+      }
+    case 'UPVOTE_YOUTUBE_VIDEO': {
+      const {payload} = action;
+      return {
+        ...youtubeVideo,
+        votes: {
+          ...youtubeVideo.votes,
+          up: youtubeVideo.votes.up + 1
+        }
+      }
+    }
+    case 'DOWNVOTE_YOUTUBE_VIDEO':
+      return {
+        ...youtubeVideo,
+        votes: {
+          ...youtubeVideo.votes,
+          down: youtubeVideo.votes.down + 1
+        }
+    }
+
+    default:
+      return youtubeVideo
+  }
+}
